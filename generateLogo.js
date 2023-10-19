2515582fb433415d8ef70dda2788a8fcd83ce203
Peaky00 <chris.kirkley@yahoo.com>
2023-10-18 22:41:52 -0500

    deleted some files

--- generateLogo.js
+++ generateLogo.js
@@ -1,67 +1,16 @@
+const svgCaptcha = require('svg-captcha');
 const fs = require('fs');
-const prompts = require('prompts');
-const { SVG, G, Text, Rect } = require('@svgdotjs/svg.js');
 
-async function generateLogo() {
-  const userInput = await prompts([
-    {
-      type: 'text',
-      name: 'text',
-      message: 'Enter up to three characters for the text:'
-    },
-    {
-      type: 'text',
-      name: 'textColor',
-      message: 'Enter text color (color keyword or hexadecimal):'
-    },
-    {
-      type: 'select',
-      name: 'shape',
-      message: 'Choose a shape:',
-      choices: ['circle', 'triangle', 'square']
-    },
-    {
-      type: 'text',
-      name: 'shapeColor',
-      message: 'Enter shape color (color keyword or hexadecimal):'
-    }
-  ]);
+// Generate a random captcha (which will serve as your logo)
+const captcha = svgCaptcha.create({
+  size: 6,          // Number of characters in the text
+  ignoreChars: '0oO1ilI', // Characters to exclude to avoid confusion
+  noise: 2,         // Number of noise lines
+  color: true,      // Enable colorful text
+  background: '#fff', // Background color
+});
 
-  const { text, textColor, shape, shapeColor } = userInput;
+// Save the generated SVG to a file
+fs.writeFileSync('logo.svg', captcha.data);
 
-  // Create an SVG document
-  const canvas = SVG().size(300, 200);
-
-  // Create a group for the logo elements
-  const group = canvas.group();
-
-  // Draw the selected shape
-  switch (shape) {
-    case 'circle':
-      group.circle(100).fill(shapeColor);
-      break;
-    case 'triangle':
-      group.polygon('50,0 0,100 100,100').fill(shapeColor);
-      break;
-    case 'square':
-      group.rect(100, 100).fill(shapeColor);
-      break;
-    default:
-      console.log('Invalid shape selection.');
-      return;
-  }
-
-  // Add the text to the logo
-  const textElement = new Text().text(text).fill(textColor).move(100, 100).font({ size: 24 });
-
-  // Append text to the group
-  group.add(textElement);
-
-  // Save the SVG to a file
-  const svgString = canvas.svg();
-  fs.writeFileSync('logo.svg', svgString);
-
-  console.log('Generated logo.svg');
-}
-
-generateLogo();
+console.log('Generated logo.svg');
